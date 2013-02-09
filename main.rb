4bf57e70f966e6725de15ffbbad73b2d8fcde464
Ron Jones <rrrron@gmail.com>
2013-02-09 18:30:05 -0500

    Set up of initial files

---
+++ main.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'active_support/all'
+require 'pg'
+
+get '/' do
+  erb :home
+end
---
+++ table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE table_name
+(
+  id serial4 primary key,
+  column_name data_type
+);
